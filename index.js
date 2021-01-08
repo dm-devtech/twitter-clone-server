e0fb3b543a93cc56b4ec94561319dd69d0d422b5
David Millen <70208036+dm-devtech@users.noreply.github.com>
2021-01-08 17:20:54 +0000

    graphql server runs locally and can be queried

---
+++ index.js
@@ -0,0 +1,24 @@
+const express = require("express")
+const { ApolloServer, gql } = require("apollo-server-express")
+const app = express()
+const port = process.env.PORT || 5000
+
+const typeDefs = gql`
+    type Query {
+      hello: String
+    }
+`
+
+const resolvers = {
+  Query: {
+    hello: () => "Hello world"
+  }
+}
+
+const server = new ApolloServer({ typeDefs, resolvers })
+
+server.applyMiddleware({ app })
+
+app.listen(port, () =>
+console.log(`Server ready at http://localhost:${port}${server.graphqlPath}`)
+)
